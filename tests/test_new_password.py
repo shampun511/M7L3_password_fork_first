8e8e977f4696adc7ac276df06e622c1f22106b18
shampun511 <den-belov00@mail.ru>
2025-05-24 19:32:57 +0500

    отчеты и доработаны тесты

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,21 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_char2():
+    valid_char = string.ascii_letters + string.digits + string.punctuation
+    n = 500
+    password = generate_password(n)
+    c = 0
+    for char in password:
+        c += 1
+    assert n == c
+
+def test_difference():
+    password1 = generate_password(100)
+    password2 = generate_password(100)
+    assert password1 != password2
+
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
